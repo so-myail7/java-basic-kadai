0ca930c355aa1326d8e45f4de6eeb6b510d9bb2e
so-myail7 <socham1397@gmail.com>
2025-01-28 17:06:59 +0900

    Add files via upload

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,20 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+	
+	public KatoHanako_Chapter18(String familyName, String address) {
+        this.familyName = familyName;
+        this.address = address;
+        setGivenName();
+	}
+	
+	public void setGivenName() {
+		
+		givenName = "花子";
+	}
+	
+	public void eachIntroduce() {
+		System.out.println("趣味は読書です");
+	}
+
+}
