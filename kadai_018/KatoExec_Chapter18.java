97bb303c7b5072f6076b9c3fff05e674e718f407
so-myail7 <socham1397@gmail.com>
2025-01-29 13:22:15 +0900

    KatoExec_Chapter18.java

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -5,11 +5,11 @@ public class KatoExec_Chapter18 {
 	
 	public static void main(String[] args) {
 		
-		KatoTaro_Chapter18 taro = new KatoTaro_Chapter18() ;
+	KatoTaro_Chapter18 taro = new KatoTaro_Chapter18() ;
 		
-		taro.setGivenName();
-		taro.execIntroduce();
-		System.out.println();	
+	taro.setGivenName();
+	taro.execIntroduce();
+	System.out.println();	
 		
         KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
         
